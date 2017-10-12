294f9a6dce56e9ce0259c8221de27588c02ec190
於杰 <alexkie@yeah.net>
2017-10-12 15:46:26 +0800

    静态文件

--- CodePedia/urls.py
+++ CodePedia/urls.py
@@ -19,7 +19,7 @@ import xadmin
 from django.views.generic import TemplateView
 from users.views import ActiveView
 from django.views.static import serve
-from CodePedia.settings import MEDIA_ROOT
+from CodePedia.settings import MEDIA_ROOT, STATIC_ROOT
 from projects.views import FileListlView, FunctionListlView
 
 urlpatterns = [
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^captcha/', include('captcha.urls')),
     # 配置上传文件的访问函数
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT,}),
     url(r'^users/', include('users.urls', namespace='users')),
     url(r'^projects/', include('projects.urls', namespace='projects')),
     # 所有文件
