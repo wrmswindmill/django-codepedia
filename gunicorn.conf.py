64ed78fe753545dc905dd919ecfbd5e897342bb1
於杰 <alexkie@yeah.net>
2017-10-31 21:53:48 +0800

    nginx

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,5 +2,5 @@ import multiprocessing
 
 bind = '127.0.0.1:8000'
 workers = 2
-errorlog = '~/ak/django-codepedia/gunicorn.error.log'
+errorlog = '/home/pdl/ak/django-codepedia/gunicorn.error.log'
 proc_name = 'gunicorn_codepedia'
